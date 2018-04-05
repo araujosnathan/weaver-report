f84040c4c95ad37cc65da792855f8e2013642997
Nathanael <nathanael@CS-571.local>
2018-04-05 16:25:48 -0300

    Add unit test for get scenario name by feature
    Signed-off-by: Nathanael <nathanael@CS-571.local>

--- scripts/functional_tests_datas.sh
+++ scripts/functional_tests_datas.sh
@@ -15,7 +15,6 @@ function get_all_features_from_testing_project
   else
     CONTENT=$(ls $PATH_TO_FEATURES | grep '.feature')
     if [ -z "$CONTENT" ]; then
-
       echo -e "\033[31;1mDo not exist any feature in folder: $PATH_TO_FEATURES \nPlease, set correct folder in config.yml!\033[m"
       exit 1
     else
@@ -32,8 +31,18 @@ function get_feature_name
 
 function get_total_number_of_scenarios_by_feature
 {
-  SCENARIOS_TOTAL_BY_FEATURE=$(cat $PATH_TO_FEATURES$LINE  | grep @$PLATFORM_NAME -A1 | grep Cenário | wc -l)
-  SCENARIOS_TOTAL_BY_FEATURE=$(echo $SCENARIOS_TOTAL_BY_FEATURE | tr -d ' ')
+    if [ -z "$PLATFORM_NAME" ]; then
+        SCENARIOS_TOTAL_BY_FEATURE=0
+    else
+        CONTENT=$(cat $PATH_TO_FEATURES$LINE  | grep @$PLATFORM_NAME)
+        if [ -z "$CONTENT" ]; then
+            echo -e "\033[31;1mThe Platform name tag: $PLATFORM_NAME was not found in '.feature' files. \nPlease, set correct platform name in config.yml and/or add this tag for all scenarios already implemented!\033[m"
+            exit 1
+        else
+            SCENARIOS_TOTAL_BY_FEATURE=$(cat $PATH_TO_FEATURES$LINE  | grep @$PLATFORM_NAME -A1 | grep 'Cenário:\|Cenario:\|Scenario:' | wc -l)
+            SCENARIOS_TOTAL_BY_FEATURE=$(echo $SCENARIOS_TOTAL_BY_FEATURE | tr -d ' ')
+        fi
+    fi
 }
 
 function get_total_number_of_scenarios_from_project
@@ -43,8 +52,12 @@ function get_total_number_of_scenarios_from_project
 
 function get_scenario_names_by_feature
 {
-  FILE_WITH_SCENARIO_NAMES_BY_FEATURE="scenarios.txt"
-  cat $PATH_TO_FEATURES$LINE  | grep @$PLATFORM_NAME -A1 | grep Cenário >> $FILE_WITH_SCENARIO_NAMES_BY_FEATURE
+    FILE_WITH_SCENARIO_NAMES_BY_FEATURE="scenarios.txt"
+    if [ -z "$PLATFORM_NAME" ]; then
+        echo "EmptyFile" >> $FILE_WITH_SCENARIO_NAMES_BY_FEATURE
+    else    
+        cat $PATH_TO_FEATURES$LINE  | grep @$PLATFORM_NAME -A1 | grep "Cenário:\|Cenario:\|Scenario:" >> $FILE_WITH_SCENARIO_NAMES_BY_FEATURE
+    fi
 }
 
 function get_total_number_of_scenarios_from_official_document_by_feature
--- scripts/tests/funcional_tests_datas_testing.sh
+++ scripts/tests/funcional_tests_datas_testing.sh
@@ -61,10 +61,97 @@ testShouldGetFeatureNameWithSpaces()
   assertEquals "Feature de Name" "$FEATURE_NAME"
 }
 
+testShouldGetTotalNumberOfScenariosinPortuguese()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME="android"
+  echo "Feature: Feature de Name" >> $PATH_TO_FEATURES$LINE
+  echo "@android" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 1 de Cenário" >> $PATH_TO_FEATURES$LINE
+  echo "@android" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 2 de Cenário" >> $PATH_TO_FEATURES$LINE
+  get_total_number_of_scenarios_by_feature
+  assertEquals "2" "$SCENARIOS_TOTAL_BY_FEATURE"
+}
+
+testShouldGetTotalNumberOfScenariosinEnglish()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME="android"
+  echo "Feature: Feature de Name" >> $PATH_TO_FEATURES$LINE
+  echo "@android" >> $PATH_TO_FEATURES$LINE
+  echo "Scenario: Exemplo 1 de Cenário" >> $PATH_TO_FEATURES$LINE
+  echo "@android" >> $PATH_TO_FEATURES$LINE
+  echo "Scenario: Exemplo 2 de Cenário" >> $PATH_TO_FEATURES$LINE
+  get_total_number_of_scenarios_by_feature
+  assertEquals "2" "$SCENARIOS_TOTAL_BY_FEATURE"
+}
+
+testShouldGetErroForNotFoundPlatformName()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME="android"
+  echo "Feature: Feature de Name" >> $PATH_TO_FEATURES$LINE
+  echo "@ios" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 1 de Cenário" >> $PATH_TO_FEATURES$LINE
+  echo "@ios" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 2 de Cenário" >> $PATH_TO_FEATURES$LINE
+  assertEquals "$(echo -e "\033[31;1mThe Platform name tag: $PLATFORM_NAME was not found in '.feature' files. \nPlease, set correct platform name in config.yml and/or add this tag for all scenarios already implemented!\033[m")" "$(get_total_number_of_scenarios_by_feature)" 
+}
+
+testShouldReturnZeroForPlatformNameEmpty()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME=""
+  echo "Feature: Feature de Name" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 1 de Cenário" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 2 de Cenário" >> $PATH_TO_FEATURES$LINE
+  get_total_number_of_scenarios_by_feature
+  assertEquals "0" "$SCENARIOS_TOTAL_BY_FEATURE"
+}
+
+testShouldCreateFileWritingEmptyFile()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME=""
+  get_scenario_names_by_feature
+  assertEquals "EmptyFile" "$(cat $FILE_WITH_SCENARIO_NAMES_BY_FEATURE)"
+}
+
+testShouldReturnFileWithScenarioNames()
+{
+  mkdir folder_test
+  touch folder_test/file_test.feature
+  PATH_TO_FEATURES="folder_test/"
+  LINE="file_test.feature"
+  PLATFORM_NAME="android"
+  echo "Feature: Feature de Name" >> $PATH_TO_FEATURES$LINE
+  echo "@android" >> $PATH_TO_FEATURES$LINE
+  echo "Cenário: Exemplo 1 de Cenário" >> $PATH_TO_FEATURES$LINE
+  get_scenario_names_by_feature
+  assertEquals "Cenário: Exemplo 1 de Cenário" "$(cat $FILE_WITH_SCENARIO_NAMES_BY_FEATURE)"
+}
+
 tearDown()
 {
   rm -rf folder_test
   rm -rf $FILE_WITH_ALL_FEATURES
+  rm -rf $FILE_WITH_SCENARIO_NAMES_BY_FEATURE
 }
 
 # load shunit2
