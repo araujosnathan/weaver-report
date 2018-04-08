acc4a94b482bb8ed472a072344a2ffadd3c3e5b4
Nathanael <nathanael@CS-571.local>
2018-04-08 02:40:05 -0300

    Delete infos
    Signed-off-by: Nathanael <nathanael@CS-571.local>

--- scripts/bugs/main.py
+++ scripts/bugs/main.py
@@ -5,14 +5,14 @@ from class_bug import Bug
 import re
 
 
-options = { 'server': 'https://projetoodete.atlassian.net/' }
-jira = JIRA(options, basic_auth=('nathanael.araujo@concrete.com.br', 'araujo123..'))
+options = { 'server': '' }
+jira = JIRA(options, basic_auth=('', ''))
 
-query_backlog = 'project = CB AND issuetype = Bug AND status != Done  AND labels = iOS AND Sprint is EMPTY ORDER BY created DESC'
-query_sprint_bug_unresolved = 'project = CB AND issuetype = Bug AND status != Done  AND labels = iOS AND Sprint in openSprints()  ORDER BY created DESC'
-query_sprint_bug_fixed = 'project = CB AND issuetype = Bug AND status = Done  AND labels = iOS AND Sprint in openSprints() ORDER BY created DESC'
-query_backlog_flagged = 'project = CB AND issuetype = Bug AND status != Done  AND Flagged = impedimento AND Sprint is EMPTY AND labels = iOS ORDER BY created DESC'
-query_sprint_bug_flagged = 'project = CB AND issuetype = Bug AND status != Done  AND Flagged = impedimento AND labels = iOS AND Sprint in openSprints() ORDER BY created DESC'
+query_backlog = ''
+query_sprint_bug_unresolved = ''
+query_sprint_bug_fixed = ''
+query_backlog_flagged = ''
+query_sprint_bug_flagged = ''
 
 bugiOS =  Bug(jira, query_backlog, query_sprint_bug_unresolved, query_sprint_bug_fixed, query_sprint_bug_flagged, query_backlog_flagged)
 print(bugiOS.get_total_backlog())
@@ -27,11 +27,11 @@ with open('bugs_flagged.txt', 'a') as the_file:
     for bugs in bugiOS.get_list_bugs_flagged():
         the_file.write('ios ' + bugs.key + " " + bugs.fields.summary + "\n")
 
-query_backlog = 'project = CB AND issuetype = Bug AND status != Done  AND labels = Android AND Sprint is EMPTY ORDER BY created DESC'
-query_sprint_bug_unresolved = 'project = CB AND issuetype = Bug AND status != Done  AND labels = Android AND Sprint in openSprints()  ORDER BY created DESC'
-query_sprint_bug_fixed = 'project = CB AND issuetype = Bug AND status = Done  AND labels = Android AND Sprint in openSprints() ORDER BY created DESC'
-query_backlog_flagged = 'project = CB AND issuetype = Bug AND status != Done  AND Flagged = impedimento AND Sprint is EMPTY AND labels = Android ORDER BY created DESC'
-query_sprint_bug_flagged = 'project = CB AND issuetype = Bug AND status != Done  AND Flagged = impedimento AND labels = Android AND Sprint in openSprints() ORDER BY created DESC'
+query_backlog = ''
+query_sprint_bug_unresolved = ''
+query_sprint_bug_fixed = ''
+query_backlog_flagged = ''
+query_sprint_bug_flagged = ''
 
 
 bugAndroid = Bug(jira, query_backlog, query_sprint_bug_unresolved, query_sprint_bug_fixed, query_sprint_bug_flagged, query_backlog_flagged)
