42fefeb41bb45f73fef0d236f9cdaca8681abd97
Nathanael <nathanael@CS-571.local>
2018-06-07 15:10:00 -0300

    Create bug graphics

---
+++ scripts/bugs/bug_data_html.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+function set_bug_platform_menu
+{
+  
+   BUG_PLATFORM_REPORT=$BUG_PLATFORM_REPORT$(echo "<li><a href="chart-morris-$PLATFORM_NAME.html">Bugs $PLATFORM_NAME</a></li>")
+}
+
+function set_chart_init
+{
+  CHART_INIT=$(echo "<script src="js/lib/morris-chart/morris-init-$PLATFORM_NAME.js"></script>")
+}
+
+function set_bugs_flagged_chart
+{
+  echo "<div class='recent-comment'>" >> $FILE_BUGS_FLAGGED
+  echo "<div class='media'>" >> $FILE_BUGS_FLAGGED
+  echo "<div class='media-left'>" >> $FILE_BUGS_FLAGGED
+  echo "<a href='#'><img alt='...' src='images/report_bug_flagged.png' class='media-object'></a>" >> $FILE_BUGS_FLAGGED
+  echo "</div>" >> $FILE_BUGS_FLAGGED
+  echo "<div class='media-body'>" >> $FILE_BUGS_FLAGGED
+  echo "<h4 class='media-heading'>$BUG_TICKET</h4>" >> $FILE_BUGS_FLAGGED
+  echo "<p>$BUG_TITLE</p>" >> $FILE_BUGS_FLAGGED
+  echo "</div>" >> $FILE_BUGS_FLAGGED
+  echo "</div>" >> $FILE_BUGS_FLAGGED
+  echo "</div>" >> $FILE_BUGS_FLAGGED
+}
+
+function set_bugs_sprints_js()
+{
+    echo "{" >> $FILE_BUGS_JS
+    echo "s: '$REPORT_NAME_LABEL'," >> $FILE_BUGS_JS
+    echo "bl: $BACKLOG_NUMBER," >> $FILE_BUGS_JS
+	echo "fx: $BUG_FIXED_NUMBER," >> $FILE_BUGS_JS
+	echo "fl: $BUG_FLAGGED_NUMBER" >> $FILE_BUGS_JS
+    if [ $COUNT -gt 1 ] && [ $MAX -lt $COUNT ];
+    then
+        echo "}," >> $FILE_BUGS_JS
+    else
+        echo "}" >> $FILE_BUGS_JS
+    fi
+}
+
+function set_chart_template()
+{
+  cat ../template/chart-morris.html >> chart-morris-$PLATFORM_NAME.html
+
+}
+
+
+function set_recent_bugs_sprint_in_html()
+{
+    FILE_BUGS=$(cat $FILE_BUGS_JS)
+    FILE_BUGS=`echo ${FILE_BUGS} | tr '\n' "\\n"`
+
+    cat ../template/js/lib/morris-chart/morris-init.js | sed -e "s|INFO_BUGS_SPRINT|${FILE_BUGS}|" > report_bugs.js
+    cat report_bugs.js | sed -e "s|BACKLOG_SPRINT|${BACKLOG_CURRENT}|" > report_bugs_1.js
+    cat report_bugs_1.js | sed -e "s|FIXED_SPRINT|${FIXED_CURRENT}|" > report_bugs.js
+    cat report_bugs.js | sed -e "s|FLAGGED_SPRINT|${FLAGGED_CURRENT}|" > ../template/js/lib/morris-chart/morris-init-$PLATFORM_NAME.js
+  
+  
+    rm -rf report_bugs.js
+    rm -rf report_bugs_1.js
+    rm -rf $FILE_BUGS_JS
+  
+}
+
+function get_all_bugs_flagged()
+{
+  cat $FILE_SPRINT_BUGS_FLAGGED | grep $PLATFORM_NAME >> sprint_historic.txt
+  while read LINE 
+  do
+    BUG_TICKET=$(echo $LINE | awk '{print $2}')
+    BUG_TITLE=$(echo $LINE | awk '{t=""; for(i=3;i<=NF;i++) t=t" "$i; print t}')
+    set_bugs_flagged_chart
+  done < sprint_historic.txt
+  rm -rf sprint_historic.txt 
+}
--- scripts/weaver-report.sh
+++ scripts/weaver-report.sh
@@ -11,6 +11,8 @@ source develop/info_html.sh
 source historic/recent_sprints.sh
 source historic/sprints_html.sh
 source jira/jira_data.sh
+source bugs/bug_data_html.sh
+source bugs/metric_data.sh
 
 function setup_envs()
 {
@@ -45,7 +47,6 @@ function check_tags_in_config_file
   if [ "$ANDROID_STATUS_UNIT_TEST" = "true" ]; then
     check_file_of_android_unit_test
   fi
-  # check_tag_jira_server
   check_tag_jira_android
   check_tag_jira_ios
 }
@@ -55,32 +56,6 @@ function set_report_platform_menu
 {
    PLATFORM_REPORT=$PLATFORM_REPORT$(echo "<li><a href="$REPORT_NAME-$PLATFORM_NAME.html">$PLATFORM_NAME</a></li>")
 }
-
-# function set_bug_platform_menu
-# {
-  
-#    BUG_PLATFORM_REPORT=$BUG_PLATFORM_REPORT$(echo "<li><a href="chart-morris-$PLATFORM_NAME.html">Bugs $PLATFORM_NAME</a></li>")
-# }
-
-# function set_chart_init
-# {
-#   CHART_INIT=$(echo "<script src="js/lib/morris-chart/morris-init-$PLATFORM_NAME.js"></script>")
-# }
-
-# function set_bugs_flagged_chart
-# {
-#   echo "<div class='recent-comment'>" >> $FILE_BUGS_FLAGGED
-#   echo "<div class='media'>" >> $FILE_BUGS_FLAGGED
-#   echo "<div class='media-left'>" >> $FILE_BUGS_FLAGGED
-#   echo "<a href='#'><img alt='...' src='images/report_bug_flagged.png' class='media-object'></a>" >> $FILE_BUGS_FLAGGED
-#   echo "</div>" >> $FILE_BUGS_FLAGGED
-#   echo "<div class='media-body'>" >> $FILE_BUGS_FLAGGED
-#   echo "<h4 class='media-heading'>$BUG_TICKET</h4>" >> $FILE_BUGS_FLAGGED
-#   echo "<p>$BUG_TITLE</p>" >> $FILE_BUGS_FLAGGED
-#   echo "</div>" >> $FILE_BUGS_FLAGGED
-#   echo "</div>" >> $FILE_BUGS_FLAGGED
-#   echo "</div>" >> $FILE_BUGS_FLAGGED
-# }
 
 function get_all_functional_datas()
 {
@@ -156,79 +131,18 @@ function get_all_contract_datas()
   fi
 }
 
-
-# function set_bugs_sprints_js()
-# {
-#   echo "{" >> $FILE_BUGS_JS
-#   echo "s: '$REPORT_NAME_LABEL'," >> $FILE_BUGS_JS
-# 	echo "bl: $BACKLOG_NUMBER," >> $FILE_BUGS_JS
-# 	echo "fx: $BUG_FIXED_NUMBER," >> $FILE_BUGS_JS
-# 	echo "fl: $BUG_FLAGGED_NUMBER" >> $FILE_BUGS_JS
-#   if [ $COUNT -gt 1 ] && [ $MAX -lt $COUNT ];
-#   then
-#     echo "}," >> $FILE_BUGS_JS
-#   else
-#     echo "}" >> $FILE_BUGS_JS
-#   fi
-# }
-
-# function get_all_sprints_bugs()
-# {
-#   cat $FILE_SPRINTS_BUGS | grep $PLATFORM_NAME >> sprint_historic.txt
-#   COUNT=$(cat $FILE_SPRINTS_BUGS | grep "$PLATFORM_NAME" | wc -l)
-#   MAX=1
-#   while read LINE 
-#   do
-#     REPORT_NAME_LABEL=$(echo $LINE | awk '{print $2}')
-#     BACKLOG_NUMBER=$(echo $LINE | awk '{print $3}')
-#     BUG_FIXED_NUMBER=$(echo $LINE | awk '{print $4}')
-#     BUG_FLAGGED_NUMBER=$(echo $LINE | awk '{print $5}')
-#     set_bugs_sprints_js
-#     let MAX++
-#   done < sprint_historic.txt
-
-#   rm -rf sprint_historic.txt
- 
-# }
-
-# function get_current_bug_metric(){
-  
-#   cat $FILE_CURRENT_BUGS_METRIC | grep "$PLATFORM_NAME" >> sprint_historic.txt
-#   while read LINE 
-#   do
-#     BACKLOG_CURRENT=$(echo $LINE | awk '{print $2}')
-#     FIXED_CURRENT=$(echo $LINE | awk '{print $3}')
-#     FLAGGED_CURRENT=$(echo $LINE | awk '{print $4}')
-#   done < sprint_historic.txt
-#   ALL_METRICS_BUGS=$ALL_METRICS_BUGS" "$BACKLOG_CURRENT" "$FIXED_CURRENT" "$FLAGGED_CURRENT
-#   rm -rf sprint_historic.txt
-# }
-
-# function get_all_bugs_flagged()
-# {
-#   cat $FILE_SPRINT_BUGS_FLAGGED | grep $PLATFORM_NAME >> sprint_historic.txt
-#   while read LINE 
-#   do
-#     BUG_TICKET=$(echo $LINE | awk '{print $2}')
-#     BUG_TITLE=$(echo $LINE | awk '{t=""; for(i=3;i<=NF;i++) t=t" "$i; print t}')
-#     set_bugs_flagged_chart
-#   done < sprint_historic.txt
-#   rm -rf sprint_historic.txt 
-# }
-
 function set_metrics_in_template()
 {
   set_functional_test_data_in_html
   set_contract_test_data_in_html
   # set_unit_test_data_in_html
   set_recent_sprints_data_in_html
- 
+
+
   # # FILE_BUGS=$(cat $FILE_BUGS_JS)
   # # FILE_BUGS=`echo ${FILE_BUGS} | tr '\n' "\\n"`
   
 
-  # cat report_tests.html | sed -e "s|ALL_SPRINTS_METRICS|${FILE_SPRINT}|" > $REPORT_NAME-$PLATFORM_NAME.html
-  
   # cat ../template/js/lib/morris-chart/morris-init.js | sed -e "s|INFO_BUGS_SPRINT|${FILE_BUGS}|" > report_bugs.js
   # cat report_bugs.js | sed -e "s|BACKLOG_SPRINT|${BACKLOG_CURRENT}|" > report_bugs_1.js
   # cat report_bugs_1.js | sed -e "s|FIXED_SPRINT|${FIXED_CURRENT}|" > report_bugs.js
@@ -243,10 +157,28 @@ function set_metrics_in_template()
   
 }
 
-function set_chart_template()
+function generate_bug_report_by_platform()
 {
-  cat ../template/chart-morris.html >> chart-morris-$PLATFORM_NAME.html
+  BUGS_FLAGGED_CONTENT=$(cat $FILE_BUGS_FLAGGED)
+  BUGS_FLAGGED_CONTENT=`echo ${BUGS_FLAGGED_CONTENT} | tr '\n' "\\n"`
 
+  cat chart-morris-$PLATFORM_NAME.html | sed -e "s|REPORT_PLATFORM_NAME|${PLATFORM_REPORT}|" > report_chart.html 
+  
+  cat report_tests.html | sed -e "s|BUGS_PLATFORM_NAME|${BUG_PLATFORM_REPORT}|" > report_tests_1.html
+  cat report_chart.html | sed -e "s|BUGS_PLATFORM_NAME|${BUG_PLATFORM_REPORT}|" > report_chart_1.html
+  set_chart_init
+  cat report_chart_1.html | sed -e "s|CHART_BUG_INIT|${CHART_INIT}|" > report_chart.html
+  cat report_chart.html | sed -e "s|LIST_FLAGGED_BUGS|${BUGS_FLAGGED_CONTENT}|" > report_chart_1.html
+  
+  rm -rf chart-morris-$PLATFORM_NAME.html
+
+  mv report_chart_1.html chart-morris-$PLATFORM_NAME.html
+  cp chart-morris-$PLATFORM_NAME.html $REPORT_NAME/
+
+  rm -rf $FILE_BUGS_FLAGGED
+  rm -rf report_tests.html
+  rm -rf report_chart_1.html
+  rm -rf report_chart.html
 }
 
 function genenerate_report_by_platform()
@@ -265,7 +197,6 @@ function genenerate_report_by_platform()
   # cat report_chart_1.html | sed -e "s|CHART_BUG_INIT|${CHART_INIT}|" > report_chart.html
   # cat report_chart.html | sed -e "s|LIST_FLAGGED_BUGS|${BUGS_FLAGGED_CONTENT}|" > report_chart_1.html
   
-  # rm -rf $REPORT_NAME-$PLATFORM_NAME.html
   # rm -rf chart-morris-$PLATFORM_NAME.html
 
   mv report_tests.html $REPORT_NAME-$PLATFORM_NAME.html
@@ -300,9 +231,7 @@ function generate_weaver_report
     ALL_METRICS=""
     ALL_METRICS=$PLATFORM_NAME
     ALL_METRICS=$ALL_METRICS" "$REPORT_NAME
-    # ALL_METRICS_BUGS=""
-    # ALL_METRICS_BUGS=$PLATFORM_NAME
-    # ALL_METRICS_BUGS=$ALL_METRICS_BUGS" "$REPORT_NAME
+    
    
     if [ "$PLATFORM_NAME" = "ios" ];
     then
@@ -310,7 +239,16 @@ function generate_weaver_report
       save_ios_unit_test
       if [ "$JIRA_IOS" = "true" ];
       then
+        ALL_METRICS_BUGS=""
+        ALL_METRICS_BUGS=$PLATFORM_NAME
+        ALL_METRICS_BUGS=$ALL_METRICS_BUGS" "$REPORT_NAME
         python3 jira/main_ios.py
+        set_bug_platform_menu
+        get_current_bug_metric
+        echo $ALL_METRICS_BUGS >> $FILE_SPRINTS_BUGS
+        get_all_sprints_bugs
+        set_chart_template
+        set_recent_bugs_sprint_in_html
       fi
     elif [ "$PLATFORM_NAME" = "android" ];
     then
@@ -318,21 +256,26 @@ function generate_weaver_report
       save_android_unit_test
       if [ "$JIRA_ANDROID" = "true" ];
       then
+        ALL_METRICS_BUGS=""
+        ALL_METRICS_BUGS=$PLATFORM_NAME
+        ALL_METRICS_BUGS=$ALL_METRICS_BUGS" "$REPORT_NAME
         python3 jira/main_android.py
+        set_bug_platform_menu
+        get_current_bug_metric
+        echo $ALL_METRICS_BUGS >> $FILE_SPRINTS_BUGS
+        get_all_sprints_bugs
+        set_chart_template
+        set_recent_bugs_sprint_in_html
       fi
     fi
  
     get_all_functional_datas
     get_all_contract_datas
     set_report_platform_menu
-    # set_bug_platform_menu
-    # get_current_bug_metric
+    
     echo $ALL_METRICS >> $FILE_WITH_RECENT_SPRINTS_METRICS
-    # echo $ALL_METRICS_BUGS >> $FILE_SPRINTS_BUGS
     get_recent_sprints_metrics
-    # get_all_sprints_bugs
-    set_metrics_in_template
-    # set_chart_template
+    set_metrics_in_template    
     let count++
     
   done
@@ -348,7 +291,7 @@ function generate_weaver_report
   cp -r ../template/ $REPORT_NAME/
   rm -rf $REPORT_NAME/index.html
   rm -rf $REPORT_NAME/chart-morris.html
-  # rm -rf $REPORT_NAME/js/lib/morris-chart/morris-init.js
+  rm -rf $REPORT_NAME/js/lib/morris-chart/morris-init.js
 
   generate_feature_express
 }
